a780fb6250c9de0f6e38c095bc1045fda97833ee
pradyumnashukla <85396659+manasshukla21@users.noreply.github.com>
2022-12-01 22:58:20 +0530

    Update Band Name generator.py

--- Band Name generator.py
+++ Band Name generator.py
@@ -1,4 +1,4 @@
-print("Welocme to the Band Name Generator") # The program welcomes you
-city_name = input("What's the name of the you grew up in? ") # The program asks you your pet name and it stores the data in a variable named city_name
-pet_name = input("Whats your pet's name?") # The program asks you your pet's name and it store the entered data in a variable named pet_name
-print(city_name +" "+ pet_name)  # The program then adds the data stored in the variables city_name and pet_name and prints the final name
+print("Welocme to the Band Name Generator")                                     # The program welcomes you
+city_name = input("What's the name of the you grew up in? ")                    # The program asks you your pet name and it stores the data in a variable named city_name
+pet_name = input("Whats your pet's name?")                              # The program asks you your pet's name and it store the entered data in a variable named pet_name
+print(city_name +" "+ pet_name)                                  # The program then adds the data stored in the variables city_name and pet_name and prints the final name
